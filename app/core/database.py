1ca0916c2b5261c885188930a092217a5e08be4e
Priyesh H. Gawali <thepriyesh17@gmail.com>
2026-09-12 16:02:39 +0530

    database connection file

---
+++ app/core/database.py
@@ -0,0 +1,17 @@
+from psycopg_pool import AsyncConnectionPool
+from app.core.config import settings
+
+pool = AsyncConnectionPool(
+    conninfo=settings.DATABASE_URL,
+    max_size=10,
+    max_idle=120,  # Shrink pool if connections are idle for 2 minutes
+    max_lifetime=300, # Forcefully replace connections every 5 minutes
+    kwargs={
+        "autocommit":True, 
+        "prepare_threshold": 0,
+        "keepalives": 1,
+        "keepalives_idle": 30,
+        "keepalives_interval": 10,
+        "keepalives_count": 5},
+    open=False,
+)
